9c8ecc39123340ea4c8d04d927a440399c7936aa
elliot townley <elliot.townley@hotmail.co.uk>
2023-01-27 12:58:55 +0000

    week2 change

---
+++ week2.py
@@ -0,0 +1,26 @@
+
+names = ["bob", "ben","jeff"]
+
+print(names)
+
+print(names[0])
+print(names[1])
+print(names[2])
+
+print(len(names))
+
+i = 0
+
+for x in names:
+    print("hello, " + x)
+
+
+
+places = ["wolvo", "dudley", "spain", "porto", "usa"]
+
+places.sort(reverse = False)
+
+print(places)
+
+
+
